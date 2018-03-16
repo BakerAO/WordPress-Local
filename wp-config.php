e4fffc26afa0d9fd576fdef2d6f3e2cb3e228c06
BakerAO <dpshunter@gmail.com>
2018-03-16 14:50:40 -0500

    Added gitignore
    Signed-off-by: BakerAO <dpshunter@gmail.com>

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_local');
+define('DB_NAME', 'dmag_local');
 
 /** MySQL database username */
 define('DB_USER', 'root');
